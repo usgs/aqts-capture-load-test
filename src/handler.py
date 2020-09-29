d3a474d57a8b3cd9262f3335a6ca3a6e456b0f4a
kkehl-usgs <kkehl@contractor.usgs.gov>
2020-09-29 09:02:19 -0700

    add create db instance

--- src/handler.py
+++ src/handler.py
@@ -5,23 +5,35 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
+DB_CLUSTER_IDENTIFIER = 'nwcapture-load'
+SNAPSHOT_IDENTIFIER = 'rds:nwcapture-qa-2020-09-27-06-15'
+DB_INSTANCE_IDENTIFIER = 'nwcapture-load-instance1'
+DB_INSTANCE_CLASS = 'db.r5.8xlarge'
+
 
 def delete_db_cluster(event, context):
     client = boto3.client('rds', os.environ['AWS_DEPLOYMENT_REGION'])
     response = client.delete_db_cluster(
-        DBClusterIdentifier='nwcapture-load',
+        DBClusterIdentifier=DB_CLUSTER_IDENTIFIER,
         SkipFinalSnapshot=True
     )
 
 
+def create_db_instance(event, context):
+    client = boto3.client('rds', os.environ['AWS_DEPLOYMENT_REGION'])
+    response = client.create_db_instance(
+        DBInstanceIdentifier=DB_INSTANCE_IDENTIFIER,
+        DBInstanceClass=DB_INSTANCE_CLASS,
+        DBClusterIdentifier=DB_CLUSTER_IDENTIFIER
+    )
+
+
 def restore_db_cluster(event, context):
-    db_cluster_identifier = 'nwcapture-load';
-    snapshot_identifier = 'rds:nwcapture-qa-2020-09-27-06-15'
     client = boto3.client('rds', os.environ['AWS_DEPLOYMENT_REGION'])
 
     response = client.restore_db_cluster_from_snapshot(
-        DBClusterIdentifier=db_cluster_identifier,
-        SnapshotIdentifier=snapshot_identifier,
+        DBClusterIdentifier=DB_CLUSTER_IDENTIFIER,
+        SnapshotIdentifier=SNAPSHOT_IDENTIFIER,
         Engine='aurora-postgresql',
         EngineVersion='11.7',
         Port=5477,
