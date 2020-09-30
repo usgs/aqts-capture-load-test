126171ffd224ca666a4b151232ab48822d6705dd
kkehl-usgs <kkehl@contractor.usgs.gov>
2020-09-30 13:33:49 -0700

    tidy up for IOW-641

--- src/handler.py
+++ src/handler.py
@@ -7,8 +7,6 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 two_days_ago = datetime.datetime.now() - datetime.timedelta(2)
 
-DB_CLUSTER_IDENTIFIER = 'nwcapture-load'
-
 month = str(two_days_ago.month)
 if len(month) == 1:
     month = f"0{month}"
@@ -20,6 +18,8 @@ SNAPSHOT_IDENTIFIER = f"rds:nwcapture-prod-external-{two_days_ago.year}-{month}-
 DB_INSTANCE_IDENTIFIER = 'nwcapture-load-instance1'
 DB_INSTANCE_CLASS = 'db.r5.8xlarge'
 ENGINE = 'aurora-postgresql'
+BUCKET_LOAD = 'iow-retriever-capture-load'
+DB_CLUSTER_IDENTIFIER = 'nwcapture-load'
 
 
 def delete_db_cluster(event, context):
@@ -73,29 +73,19 @@ def create_db_instance(event, context):
     }
 
 
+def delete_bucket(event, context):
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket('my-bucket')
+    bucket.objects.all().delete()
+    client = boto3.client('s3')
+    response = client.delete_bucket(
+        Bucket=BUCKET_LOAD
+    )
+
+
 def copy_s3(event, context):
     # TODO modify aqts-capture-trigger to have a fake trigger bucket that works the same
     # as the real trigger bucket (name: aqts-retriever-capture-load)
-    pass
-
-
-def copy_s3_temp(event, context):
-    # TODO modify aqts-capture-trigger to have a fake trigger bucket that works the same
-    # as the real trigger bucket (name: aqts-retriever-capture-load)
-
-    # s3 = boto3.resource('s3')
-    # src_bucket = s3.Bucket('iow-retriever-capture-reference')
-    # dest_bucket = s3.Bucket('iow-retriever-capture-load')
-    # dest_bucket.objects.all().delete()
-    # count = 0
-    # for obj in src_bucket.objects.all():
-    #     logger.debug(f"found {obj.key} in src_bucket")
-    #     s3.Object(dest_bucket, obj.key).put(Body=obj.get()["Body"].read())
-    #     count = count + 1
-    # return {
-    #     'statusCode': 200,
-    #     'message': f"copy_s3 copied {count} objects"
-    # }
 
     s3_client = boto3.client('s3')
     resp = s3_client.list_objects_v2(Bucket='iow-retriever-capture-reference')
@@ -110,7 +100,7 @@ def copy_s3_temp(event, context):
             'Bucket': 'iow-retriever-capture-reference',
             'Key': key
         }
-        bucket = s3_resource.Bucket('iow-retriever-capture-load')
+        bucket = s3_resource.Bucket(BUCKET_LOAD)
         bucket.copy(copy_source, key)
         count = count + 1
     return {
@@ -145,3 +135,27 @@ def restore_db_cluster(event, context):
         'statusCode': 201,
         'message': f"Db cluster should be restored {response}"
     }
+
+
+def disable_main_trigger(event, context):
+    logger.debug(
+        "Here we will disable the main trigger on the real bucket.  See aqts-capture-ecosystem-switch.")
+
+
+def enable_main_trigger(event, context):
+    logger.debug(
+        "Here we will enable the main trigger on the real bucket.  See aqts-capture-ecosystem-switch.")
+
+
+def falsify_secrets(event, context):
+    logger.debug(
+        "Here we will modify the secrets for nwcapture-test so that lambdas talk to nwcapture-load.")
+
+
+def restore_secrets(event, context):
+    logger.debug(
+        "Here we will restore the secrets for nwcapture-test.")
+
+
+def run_integration_tests(event, context):
+    logger.debug("Here we will run tests and report results.")
